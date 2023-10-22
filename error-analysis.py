e8673b5844e19003ff7b4937c414b2632bff2ca3
MoritzLaurer <moritz.laurer@posteo.de>
2023-10-22 16:43:10 +0200

    output analyses updates for v1

--- error-analysis.py
+++ error-analysis.py
@@ -44,7 +44,7 @@ for data_dic in data_dic_lst:
     experiment_metadata = {key: value for key, value in data_dic["experiment_metadata"].items() if key in experiment_metadata_to_keep}
 
     group_col = data_dic["experiment_metadata"]["group_col"]
-    group_member = data_dic["experiment_metadata"]["group_members"]
+    group_member = str(data_dic["experiment_metadata"]["group_members"])
 
     # Perform the chi-square test
     df_crosstab = pd.crosstab(df_test[group_col], df_test['error'])
@@ -53,29 +53,97 @@ for data_dic in data_dic_lst:
     # logistic regression
     # one-hot encoding for group_member used for sampling/biasing
     if group_member != "randomall":
-        try:
-            onehot_group_member = [0 if x == group_member else 1 for x in df_test[group_col]]
+        #try:
+            onehot_group_member = [1 if str(x) == group_member else 0 for x in df_test[group_col]]
             model = sm.Logit(df_test['error_int'], sm.add_constant(onehot_group_member)).fit()
             coefficients = model.params["x1"]
             p_values = model.pvalues["x1"]
-        except Exception as e:
-            print(f"\n\nIssue with {group_col} {group_member}. Error:\n{e}\n\n")
-            continue
+        #except Exception as e:
+        #    print(f"\n\nIssue with {group_col} {group_member}. Error:\n{e}\n\n")
+        #    raise e
+        #    continue
     else:
         coefficients = np.nan
         p_values = np.nan
 
     # append
-    error_dic_lst.append({**experiment_metadata, "chi2": chi2, "p-chi2": p, "coef-reg": coefficients, "p-reg": p_values})
+    error_dic_lst.append({**experiment_metadata, "chi2": chi2, "p-chi2": p, "coef-reg": coefficients, "oddsratio-reg": np.exp(coefficients), "p-reg": p_values})
 
 df_errors = pd.DataFrame(error_dic_lst)
 
+# additional cleaning
+method_map = {
+    "classical_ml": "logistic reg.", "standard_dl": "BERT-base", "nli_short": "BERT-NLI"
+}
+df_errors["method"] = df_errors["method"].map(method_map)
+group_map = {
+    'parfam_text': "party fam", 'country_iso': "country", 'decade': "decade",
+    'year': "year", 'continent': "continent", 'ISO_A3': "country",
+    'domain': "domain", 'pres_party': "party", 'phase': "phase"
+}
+df_errors["group_col"] = df_errors["group_col"].map(group_map)
+dataset_map = {
+    'pimpo': "PImPo", 'coronanet': "CoronaNet", 'cap-merge': "CAP-SotU+Court", 'cap-sotu': "CAP-SotU"
+}
+df_errors["dataset"] = df_errors["dataset"].map(dataset_map)
+df_errors = df_errors.rename(columns={"group_col": "group"})
 
+
+# sort values
+df_errors.loc[:,'method'] = pd.Categorical(
+    df_errors['method'], categories=["logistic reg.", "BERT-base", "BERT-NLI"], ordered=True
+)
+df_errors.loc[:,'dataset'] = pd.Categorical(
+    df_errors['dataset'], categories=['PImPo', 'CoronaNet', 'CAP-SotU', 'CAP-SotU+Court'], ordered=True
+)
+
+# remove nli_void for now
+df_errors = df_errors[df_errors["method"] != "nli_void"]
+
+# remove randomall, because regression not properly possible and already doing randomall vs. biased comparison in absolute f1 macro analysis
+df_errors = df_errors[df_errors["group_sample_strategy"] != "randomall"]
+
+
+## results disaggregated
 # inspect results
-df_errors_grouped = df_errors.groupby(["method", "group_col", "group_sample_strategy" ], as_index=False)[["chi2", "p-chi2", "coef-reg", "p-reg"]].mean().round(3)
+df_errors_grouped = df_errors.groupby(["method", "group", "group_sample_strategy"], as_index=False)[["chi2", "p-chi2", "coef-reg", "oddsratio-reg", "p-reg"]].mean().round(2)
+# add dataset column again
+group_dataset_map = dict(zip(df_errors['group'], df_errors['dataset']))
+df_errors_grouped.insert(0, "dataset", df_errors_grouped["group"].map(group_dataset_map))
+# move method column
+col_to_move = df_errors_grouped.pop('method')
+df_errors_grouped.insert(2, 'method', col_to_move)
+# sort
+df_errors_grouped = df_errors_grouped.sort_values(["dataset", "group", "method"])
+df_errors_grouped["chi2"] = df_errors_grouped["chi2"].round(1)
 
-df_errors_grouped_aggreg = df_errors.groupby(["method", "group_sample_strategy"], as_index=False)[["chi2", "p-chi2", "coef-reg", "p-reg"]].mean().round(3)
+## results aggregated
+df_errors_grouped_aggreg = df_errors.groupby(["method", "group_sample_strategy"], as_index=False)[["chi2", "p-chi2", "coef-reg", "oddsratio-reg", "p-reg"]].mean().round(2)
+df_errors_grouped_aggreg["chi2"] = df_errors_grouped_aggreg["chi2"].round(1)
 
 print(df_errors_grouped)
 print(df_errors_grouped_aggreg)
+
+
+
+
+# save to disk
+save_to_disk = True
+if save_to_disk:
+    df_errors_grouped.to_excel("./viz/error-analysis-disaggreg.xlsx", index=False)
+    df_errors_grouped_aggreg.to_excel("./viz/error-analysis-aggreg.xlsx", index=False)
+
+
+### example interpretation
+# GPT4 conversation: https://chat.openai.com/share/c6920a60-1342-4afe-a893-49a383aa97e6
+## coefficients:
+# coefficients are negative. this means that for the favoured group in biased df_train, the odds of making an error are lower than for the other groups not in df_train.
+# the coefficients are more negative the less transfer learning. this means that less transfer learning means more bias.
+
+## odds ratio: e.g. 0.81 for nli, 0.7 for classical_ml.
+# For NLI this means: The odds of an error occurring when the group is the biased group are 0.81 times the odds when the group is not the biased group.
+# More intuitively: The odds of an error occurring when the group is the biased group are 19% lower than when the group is not the biased group.
+# for classical_ml: The odds of an error occurring when the group is the biased group are 30% lower than when the group is not the biased group.
+# the odds of making an error would be equal if odds ratio == 1 or higher if odds ratio > 1.
+# => both are biased in favour of the df_train group. classical_ml is even more biased. It is less likely to make an error on the biased group than NLI.
 
--- metrics-viz.py
+++ metrics-viz.py
@@ -41,17 +41,36 @@ df_results.drop(columns=[
     "seed_run", "n_run", "train_time"
     ], inplace=True)
 
+# additional cleaning
+method_map = {
+    "classical_ml": "logistic reg.", "standard_dl": "BERT-base", "nli_short": "BERT-NLI"
+}
+df_results["method"] = df_results["method"].map(method_map)
+group_map = {
+    'parfam_text': "party fam", 'country_iso': "country", 'decade': "decade",
+    'year': "year", 'continent': "continent", 'ISO_A3': "country",
+    'domain': "domain", 'pres_party': "party", 'phase': "phase"
+}
+df_results["group_col"] = df_results["group_col"].map(group_map)
+dataset_map = {
+    'pimpo': "PImPo", 'coronanet': "CoronaNet", 'cap-merge': "CAP-SotU+Court", 'cap-sotu': "CAP-SotU"
+}
+df_results["dataset"] = df_results["dataset"].map(dataset_map)
+df_results = df_results.rename(columns={"group_col": "group"})
 
 
-AGGREGATE_METHOD_ONLY = False
+
+AGGREGATE_METHOD_ONLY = True
 if AGGREGATE_METHOD_ONLY:
-    variables_to_group_by = ["method"]  # ["dataset", "group_col", "method"] ["method"]
+    variables_to_group_by = ["method"]  # ["dataset", "group", "method"] ["method"]
 else:
-    variables_to_group_by = ["dataset", "group_col", "method"]  #["dataset", "group_col", "method"] ["method"]
+    variables_to_group_by = ["dataset", "group", "method"]  #["dataset", "group", "method"] ["method"]
 
 # remove nli_void for now
 df_results = df_results[df_results["method"] != "nli_void"]
 df_results = df_results[df_results["sample_size_train"] == 500]
+
+
 
 # differentiate between "sample_size_train" ?
 df_results_mean = df_results.groupby(["group_sample_strategy"] + variables_to_group_by).mean().reset_index()
@@ -74,10 +93,15 @@ df_merged = pd.merge(df_results_mean_random1, df_results_mean_randomall, on=vari
 
 # order values
 df_merged.loc[:,'method'] = pd.Categorical(
-    df_merged['method'], categories=["classical_ml", "standard_dl", "nli_short"], ordered=True
+    df_merged['method'], categories=["BERT-NLI", "BERT-base", "logistic reg."], ordered=True
 )
-
-df_merged = df_merged.sort_values(variables_to_group_by)  #(["dataset", "group_col", "method"])
+#df_merged.loc[:,'dataset'] = pd.Categorical(
+#    df_merged['dataset'], categories=['PImPo', 'CoronaNet', 'CAP-SotU', 'CAP-SotU+Court'], ordered=True
+#)
+#df_merged.loc[:,'group'] = pd.Categorical(
+#    df_merged['group'], categories=["BERT-NLI", "BERT-base", "logistic reg."], ordered=True
+#)
+df_merged = df_merged.sort_values(variables_to_group_by)  #(["dataset", "group", "method"])
 
 
 
@@ -89,9 +113,9 @@ import numpy as np
 
 # Sample data
 if AGGREGATE_METHOD_ONLY:
-    categories = df_merged.method.astype(str) #+ " - " + df_merged.group_col + " - " + df_merged.dataset
+    categories = df_merged.method.astype(str) #+ " - " + df_merged.group + " - " + df_merged.dataset
 else:
-    categories = df_merged.method.astype(str) + " - " + df_merged.group_col + " - " + df_merged.dataset
+    categories = df_merged.method.astype(str) + " - " + df_merged.group.astype(str) + " - " + df_merged.dataset.astype(str)
 
 
 # Y-axis positions for categories
@@ -99,7 +123,7 @@ y_positions = np.arange(len(categories))
 
 # Set the size and style of the figure
 if AGGREGATE_METHOD_ONLY:
-    plt.figure(figsize=(12, 4))
+    plt.figure(figsize=(12, 5))
 else:
     plt.figure(figsize=(12, 10))
 
@@ -111,18 +135,18 @@ plt.style.use('seaborn-whitegrid')  # Use seaborn's whitegrid style
 
 if not AGGREGATE_METHOD_ONLY:
     # Get the errorbar object for 'random 1'
-    line1, caplines1, barlinecols1 = plt.errorbar(df_merged.eval_f1_macro_random1, y_positions, xerr=df_merged.eval_f1_macro_std_random1, fmt='o', color='r', label='random 1')
+    line1, caplines1, barlinecols1 = plt.errorbar(df_merged.eval_f1_macro_random1, y_positions, xerr=df_merged.eval_f1_macro_std_random1, fmt='o', color='r', label='data_train biased')
     # Make the error bar for 'random 1' dotted
     for bar in barlinecols1:
         bar.set_linestyle('--')
     # Get the errorbar object for 'random all'
-    line2, caplines2, barlinecols2 = plt.errorbar(df_merged.eval_f1_macro_randomall, y_positions, xerr=df_merged.eval_f1_macro_std_randomall, fmt='o', color='b', label='random all')
+    line2, caplines2, barlinecols2 = plt.errorbar(df_merged.eval_f1_macro_randomall, y_positions, xerr=df_merged.eval_f1_macro_std_randomall, fmt='o', color='b', label='data_train random')
     # Make the error bar for 'random all' dotted
     for bar in barlinecols2:
         bar.set_linestyle(':')
 else:
-    plt.scatter(df_merged.eval_f1_macro_random1, y_positions, color='r', label='random 1', marker='o')
-    plt.scatter(df_merged.eval_f1_macro_randomall, y_positions, color='b', label='random all', marker='o')
+    plt.scatter(df_merged.eval_f1_macro_random1, y_positions, color='r', label='data_train biased', marker='o')
+    plt.scatter(df_merged.eval_f1_macro_randomall, y_positions, color='b', label='data_train random', marker='o')
 
 # Add arrows
 if AGGREGATE_METHOD_ONLY:
@@ -133,11 +157,19 @@ if AGGREGATE_METHOD_ONLY:
         difference = x2 - x1
         plt.text((x1 + x2) / 2, y, f'{difference:.3f}', ha='center', va='bottom', fontsize=13, color='black')
 
-plt.yticks(y_positions, categories, fontsize=12)  # Adjusted font size
-plt.xlabel('X label', fontsize=14)  # Adjusted font size
-plt.ylabel('Methods and Groups', fontsize=14)  # Added y-label with adjusted font size
-plt.title('Title', fontsize=16)  # Adjusted font size
-plt.legend(loc='upper left', fontsize=12)  # Adjusted font size
+plt.xlabel('F1 Macro', fontsize=14)  # Adjusted font size
+
+if AGGREGATE_METHOD_ONLY:
+    plt.yticks(y_positions, categories, fontsize=14)  # Adjusted font size
+    plt.legend(loc='upper right', fontsize=12)  # Adjusted font size
+    #plt.ylabel('Methods', fontsize=14)  # Added y-label with adjusted font size
+    plt.ylim(y_positions[0] - 0.5, y_positions[-1] + 0.5)
+    plt.title(f'Average performance ({len(df_results.dataset.unique())} datasets, {len(df_results.group.unique())+1} groups) & bias penalty', fontsize=16)  # Adjusted font size
+else:
+    plt.yticks(y_positions, categories, fontsize=12)  # Adjusted font size
+    plt.legend(loc='upper right', fontsize=12)  # Adjusted font size
+    plt.ylabel('Methods & Datasets & Groups', fontsize=14)  # Added y-label with adjusted font size
+    plt.title(f'Disaggregated performance on biased and unbiased data ({len(df_results.dataset.unique())} datasets, {len(df_results.group.unique())+1} groups)', fontsize=16)  # Adjusted font size
 
 # Add horizontal and vertical grid lines
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
