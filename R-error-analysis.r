82ef59f48cde99a0666c5c46638ebcdc9519783a
MoritzLaurer <moritz.laurer@posteo.de>
2023-10-29 18:21:20 +0100

    add R scripts

---
+++ R-error-analysis.r
@@ -0,0 +1,96 @@
+library(lme4)
+library(sjPlot)
+library(tidyverse)
+library(stringr)
+library(arrow)
+
+setwd("/Users/moritzlaurer/Dropbox/PhD/Papers/meta-metrics/meta-metrics-repo/")
+
+d = read_parquet("./results/df_test_concat.parquet.gzip") |>
+  # this analysis is only conducted on experiments with biased training data
+  filter(data_train_biased == TRUE) |>
+  # new columns: biased_row is 1 if the text in df_test came from the biased group df_train was sampled from
+  # using (partial) string matching here because group_members_train are 3 countries like "USA|DEU|FRA" and group_members_test is only one string like "USA"
+  #mutate(biased_row = as.numeric(group_members_test == group_members_train), error = as.numeric(label_pred != label_gold)) |>
+  mutate(biased_row = as.numeric(str_detect(group_members_train, fixed(group_members_test)))) |>
+  # add error column
+  mutate(error = as.numeric(label_pred != label_gold)) |>
+  # clean classifier names
+  mutate(classifier = recode(method, nli_short = "BERT-NLI", nli_void = "BERT-NLI-void", standard_dl = "BERT-base", "classical_ml" = "logistic reg.")) |>
+  mutate(training_run = file_name) 
+  
+
+# TODO: check why:
+#coronanet - continent & year raise warning with glmr: boundary (singular) fit: see help('isSingular')
+#test <- d %>% filter(dataset %in% c("coronanet") & group_col %in% c("continent"))
+
+
+# The ordering here decides which method is the reference method
+# take BERT-NLI as reference category, since main argument in paper is about NLI.
+d$classifier <- factor(as.factor(d$classifier), levels = c("BERT-NLI", "BERT-NLI-void", "BERT-base", "logistic reg."))
+
+
+### single model without intercept
+m_single_nointercept = glmer(error ~ -1 + classifier + classifier:biased_row + (1 | training_run), family=binomial, data=d)
+tab_model(m_single_nointercept)
+summary(m_single_nointercept)
+plot_single_easier = plot_model(m_single_nointercept, type='pred', terms=c('classifier','biased_row'))
+plot_single_easier
+
+# Extract fixed effects coefficients
+coefficients <- fixef(m_single_nointercept)
+# Calculate odds ratios
+odds_ratios <- exp(coefficients)
+# probabilities without bias
+odds_ratio_to_prob <- function(x) x / (x+1)
+sapply(odds_ratios[1:4],  odds_ratio_to_prob)
+# probabilities with bias
+sapply(odds_ratios[1:4] * odds_ratios[5:8],  odds_ratio_to_prob)
+
+
+
+### model per data + group var
+models = list()
+for (dataset in unique(d$dataset)) {
+  for (group_col in unique(d$group_col)) {
+    ds = d[d$dataset == dataset & d$group_col == group_col,]
+    if (nrow(ds) == 0) next
+    label = paste0(dataset, ' - ', group_col)
+    message(label)
+    #models[[label]] = glmer(error ~ classifier*biased_row + (1 | training_run), family=binomial, data=ds)
+    models[[label]] = glmer(error ~ -1 + classifier + classifier:biased_row + (1 | training_run), family=binomial, data=ds)
+    
+  }
+}
+tab_model(models, dv.labels = names(models), show.ci=F, show.se=F, p.style='stars')
+
+plots = list()
+for (model in names(models)) {
+  plots[[model]] = plot_model(models[[model]], type='pred', title=model, terms=c('classifier','biased_row'))
+}
+plot_grid(plots, tags=rep('', length(plots)))
+
+
+
+
+
+### single model with intercept
+# not used in paper
+
+m_single = glmer(error ~ classifier*biased_row + (1 | training_run), family=binomial, data=d)
+tab_model(m_single)
+summary(m_single)
+plot_single <- plot_model(m_single, type='pred', terms=c('classifier','biased_row'))
+plot_single
+
+
+# export model outputs for paper
+library(stargazer)
+stargazer(m_single, type="text")
+library(broom.mixed)
+tidy_output <- tidy(m_single)
+glance_output <- glance(m_single)
+
+
+
+
---
+++ R-viz-biaspenalty.R
@@ -0,0 +1,149 @@
+library(tidyverse)
+
+setwd("/Users/moritzlaurer/Dropbox/PhD/Papers/meta-metrics/meta-metrics-repo/")
+
+cisize = function(group, value) {
+  groups = unique(group)
+  if (length(groups) != 2) stop("There should be exactly two groups")
+  x = value[group == groups[1]]
+  y = value[group == groups[2]]
+  if (length(x) == 1 | length(y) == 1) {
+    warning(glue::glue("Not enough cases: |x|={length(x)}, |y|={length(y)}"))
+    m = c(meandiff=mean(x) - mean(y), lwr.ci=NA, upr.ci=NA)
+  } else {
+    m = DescTools::MeanDiffCI(x, y)
+  }
+  as_tibble(as.list(m))
+}
+
+
+d = read_csv("./results/df_results.csv.gz") |> 
+  mutate(method=fct_reorder(method, eval_f1_macro, .fun=mean, .desc = T),
+         bias=ifelse(group_sample_strategy=="randomall", "Random", "Biased")) |>
+  select(task, group, method, bias, f1_macro=eval_f1_macro) |> 
+  #filter(!is.na(method)) |> 
+  group_by(task, group, method) |>
+  arrange(task, group, method, bias) |>
+  #mutate(f1_macro_dev=f1_macro - mean(f1_macro))  |>
+  ungroup()
+
+
+
+### create disaggregated plot
+
+d2 = d |>
+  group_by(task, group, method, bias) |> 
+  summarize(sd=sd(f1_macro), f1_macro=mean(f1_macro), n=n()) 
+
+d3b <- d |> 
+  group_by(task, group, method) |>
+  summarize(x=cisize(fct_rev(bias), f1_macro)) |> 
+  unnest_wider(x)
+
+d4 = rbind(add_column(d2, f="F1 macro"),
+           add_column(d3b, bias="diff", f="Bias penalty")) |>
+     mutate(f=fct_rev(f))
+
+
+plot_disaggregated <- ggplot(d4, aes(x=f1_macro, y=method, color=bias)) + 
+  geom_point(data=filter(d4, bias=="diff"), aes(x=meandiff)) + 
+  geom_point(data=filter(d4, bias=="Random"), position=position_nudge(y=.1)) + 
+  geom_point(data=filter(d4, bias=="Biased"), position=position_nudge(y=-.1)) + 
+  geom_segment(data=filter(d4, bias=="Random"), 
+               position=position_nudge(y=.1),
+               aes(x=f1_macro-sd, xend=f1_macro+sd, yend=method)) + 
+  geom_vline(data=filter(d4, bias=="diff") |> add_column(x=0), 
+             mapping=aes(xintercept=x), color="grey", lty=2) + 
+  geom_segment(data=filter(d4, bias=="diff"),
+               aes(x=lwr.ci, xend=upr.ci, yend=method)) + 
+  geom_segment(data=filter(d4, bias=="Biased"), 
+               position=position_nudge(y=-.1),
+               aes(x=f1_macro-sd, xend=f1_macro+sd, yend=method)) + 
+  ggh4x::facet_nested(task + group ~ f, scales = "free", space = "free") + 
+  theme_classic() + 
+  scale_color_discrete(name="Training data sampling strategy", breaks=c("Biased", "Random")) + 
+  theme(panel.grid.major.y = element_line(),
+        legend.position = "bottom") + 
+  xlab("") + 
+  ylab("")
+
+plot_disaggregated
+plot_disaggregated_data <- plot_disaggregated$data
+
+
+
+
+### create aggregated plot
+d2t = d |>
+  group_by(task) |>
+  mutate(f1_macro_b=f1_macro-mean(f1_macro)) |> 
+  group_by(method, bias) |> 
+  summarize(sd=sd(f1_macro_b), f1_macro_b=mean(f1_macro_b), f1_macro=mean(f1_macro), n=n()) 
+
+
+d3t <- d |> 
+  group_by(task) |>
+  mutate(f1_macro=f1_macro-mean(f1_macro)) |> 
+  group_by(method) |>
+  summarize(x=cisize(fct_rev(bias), f1_macro)) |> 
+  unnest_wider(x)
+
+d5 = rbind(
+  add_column(d2t, f="F1 macro relative to task mean", g="F1 macro averaged over all datasets and groups",
+             task="A total", group="A total"),
+  add_column(d3t, f="Bias penalty", g=f, bias="diff", task="A total", group="A total")
+) |>
+  mutate(f=fct_rev(f), g=fct_rev(g))
+
+# Using global mean, but confidence interval based on task-centered means
+plot_aggregated <- ggplot(d5, aes(x=f1_macro, y=method, color=bias)) + 
+  geom_point(data=filter(d5, bias=="diff"), aes(x=meandiff)) + 
+  geom_point(data=filter(d5, bias=="Random"), position=position_nudge(y=.1)) + 
+  geom_point(data=filter(d5, bias=="Biased"), position=position_nudge(y=-.1)) + 
+  geom_segment(data=filter(d5, bias=="Random"), 
+               position=position_nudge(y=.1),
+               aes(x=f1_macro-sd, xend=f1_macro+sd, yend=method)) + 
+  geom_vline(data=filter(d5, bias=="diff") |> add_column(x=0), 
+             mapping=aes(xintercept=x), color="grey", lty=2) + 
+  geom_segment(data=filter(d5, bias=="diff"),
+               aes(x=lwr.ci, xend=upr.ci, yend=method)) + 
+  geom_segment(data=filter(d5, bias=="Biased"), 
+               position=position_nudge(y=-.1),
+               aes(x=f1_macro-sd, xend=f1_macro+sd, yend=method)) + 
+  ggh4x::facet_nested(. ~ g, scales = "free", space = "free") + 
+  theme_classic() + 
+  scale_color_discrete(name="Training data sampling strategy", breaks=c("Biased", "Random")) + 
+  theme(panel.grid.major.y = element_line(),
+        legend.position = "bottom") + 
+  xlab("") + 
+  ylab("")
+
+plot_aggregated
+plot_aggregated_data <- plot_aggregated$data
+
+
+# Using task-centered means
+plot_aggregated_relative <- ggplot(d5, aes(x=f1_macro_b, y=method, color=bias)) + 
+  geom_point(data=filter(d5, bias=="diff"), aes(x=meandiff)) + 
+  geom_point(data=filter(d5, bias=="Random"), position=position_nudge(y=.1)) + 
+  geom_point(data=filter(d5, bias=="Biased"), position=position_nudge(y=-.1)) + 
+  geom_segment(data=filter(d5, bias=="Random"), 
+               position=position_nudge(y=.1),
+               aes(x=f1_macro_b-sd, xend=f1_macro_b+sd, yend=method)) + 
+  geom_vline(data=filter(d5, bias=="diff") |> add_column(x=0), 
+             mapping=aes(xintercept=x), color="grey", lty=2) + 
+  geom_vline(xintercept=0, color="grey", lty=2) + 
+  geom_segment(data=filter(d5, bias=="diff"),
+               aes(x=lwr.ci, xend=upr.ci, yend=method)) + 
+  geom_segment(data=filter(d5, bias=="Biased"), 
+               position=position_nudge(y=-.1),
+               aes(x=f1_macro_b-sd, xend=f1_macro_b+sd, yend=method)) + 
+  ggh4x::facet_nested(. ~ f, scales = "free", space = "free") + 
+  theme_classic() + 
+  scale_color_discrete(name="Training data sampling strategy", breaks=c("Biased", "Random")) + 
+  theme(panel.grid.major.y = element_line(),
+        legend.position = "bottom") + 
+  xlab("") + 
+  ylab("")
+
+
